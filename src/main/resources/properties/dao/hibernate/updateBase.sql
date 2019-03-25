569e4e9ff0a66e6992b6ab3403103561bcba7f42
Benjamin SECLIER <benjamin.seclier@univ-lorraine.fr>
2019-03-25 11:48:54 +0100

    Update updateBase.sql
    Recopie fichier de la branche AMU_IHM (plus complet)

--- src/main/resources/properties/dao/hibernate/updateBase.sql
+++ src/main/resources/properties/dao/hibernate/updateBase.sql
@@ -1,2 +1,5 @@
 UPDATE `h_acti` SET scop='INVITED' where scop='DEFAULT';
+UPDATE `h_cate` SET cate_confi = false;
 UPDATE `h_cate` SET cate_invi = false;
+UPDATE `h_depa` SET confi = false;
+UPDATE `h_tick` SET anon = false;
