82c5d5c3d6d4d2b52a8ede2d855105c3725ed016
Guillaume COLSON <guillaume.colson@univ-lorraine.fr>
2019-03-26 17:31:43 +0100

    Petits Correctifs et paramètres plus génériques

--- src/main/resources/properties/dao/hibernate/updateBase-v3tov4.sql
+++ src/main/resources/properties/dao/hibernate/updateBase-v3tov4.sql
@@ -2,4 +2,5 @@ UPDATE `h_acti` SET scop='INVITED' where scop='DEFAULT';
 UPDATE `h_cate` SET cate_confi = false;
 UPDATE `h_cate` SET cate_invi = false;
 UPDATE `h_depa` SET confi = false;
+UPDATE `h_depa` SET anon = false;
 UPDATE `h_tick` SET anon = false;
