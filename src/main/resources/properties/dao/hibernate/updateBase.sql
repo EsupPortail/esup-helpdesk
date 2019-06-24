e56b5bb1a6d99e92c210717da6dc7c9e28c917d5
Guillaume COLSON <guillaume.colson@univ-lorraine.fr>
2019-06-24 10:14:15 +0200

    petits correctifs et params plus génériques

--- src/main/resources/properties/dao/hibernate/updateBase.sql
+++ src/main/resources/properties/dao/hibernate/updateBase.sql
@@ -3,4 +3,5 @@ UPDATE `h_depa` SET visi='' where visi IS NULL;
 UPDATE `h_cate` SET cate_confi = false;
 UPDATE `h_cate` SET cate_invi = false;
 UPDATE `h_depa` SET confi = false;
+UPDATE `h_depa` SET anon = false;
 UPDATE `h_tick` SET anon = false;
